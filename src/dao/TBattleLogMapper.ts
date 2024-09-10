d13584c9a1ca1999a79c3241e8aee432f241ef7c
yonen <purple2000tatu@yahoo.co.jp>
2024-09-10 21:38:48 +0900

    初期コミット

---
+++ src/dao/TBattleLogMapper.ts
@@ -0,0 +1,62 @@
+import Database from "better-sqlite3";
+import { DB_FILE_DIR } from "../util/dbConfig";
+import { TBattleLog } from "./model";
+
+const db = new Database(DB_FILE_DIR);
+
+export const TBattleLogMapper = {
+  /** 全件取得 */
+  selectAll: (): TBattleLog[] => {
+    const result = db.prepare("select * from T_BATTLE_LOG").all();
+
+    return result.map((row: any) => {
+      return {
+        logId: row.LOG_ID,
+        battleDate: row.BATTLE_DATE,
+        season: row.SEASON,
+        myDeck: row.MY_DECK,
+        oppositeDeck: row.OPPOSITE_DECK,
+        coinToss: row.COIN_TOSS,
+        winLose: row.WIN_LOSE,
+        rank: row.RANK,
+      };
+    });
+  },
+
+  /** PKから１レコード取得 */
+  selectByPK: (id: number): TBattleLog => {
+    const result: any = db
+      .prepare("select * from T_BATTLE_LOG where LOG_ID = ?")
+      .get(id);
+
+    const response: TBattleLog = {
+      logId: result.LOG_ID,
+      battleDate: result.BATTLE_DATE,
+      season: result.SEASON,
+      myDeck: result.MY_DECK,
+      oppositeDeck: result.OPPOSITE_DECK,
+      coinToss: result.COIN_TOSS,
+      winLose: result.WIN_LOSE,
+      rank: result.RANK,
+    };
+
+    return response;
+  },
+
+  /** レコード追加 */
+  insert: (input: TBattleLog): TBattleLog => {
+    const result = db
+      .prepare("insert into T_BATTLE_LOG values(?, ?, ?, ?, ?, ?, ?, ?)")
+      .run(
+        null,
+        input.battleDate,
+        input.season,
+        input.myDeck,
+        input.oppositeDeck,
+        input.coinToss,
+        input.winLose,
+        input.rank
+      );
+    return { ...input, logId: result.lastInsertRowid as number };
+  },
+};
---
+++ src/util/dbConfig.ts
@@ -0,0 +1,2 @@
+/** DBファイルのフォルダパス */
+export const DB_FILE_DIR = "../sqlite/yu-gi-oh-track-record.sqlite3";
